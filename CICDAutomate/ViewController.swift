480ec39de0bb2bf1ca2f93fb338496d4462b51d6
nilu.walmik@tissatech.com <nilu.walmik@tissatech.com>
2024-03-08 17:33:53 +0530

    updated files

--- CICDAutomate/ViewController.swift
+++ CICDAutomate/ViewController.swift
@@ -11,7 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-       print("CICD PIPELINE")
+       print("CICD PIPELINE AUTOMATION")
     }
 
 
